9b7a9daeef6f482bd8660fdb92b8fba2ba9593b3
Veer Gadodia <veergadodia24@gmail.com>
2020-12-19 11:42:47 -0500

    restructured svg

--- depthx/screens/Signin.js
+++ depthx/screens/Signin.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SvgComponent from "./SvgComponent"
+import SvgComponent from "../svg/SvgComponent"
 import AppText from "../components/AppText"
 import SigninButton from "../components/SigninButton"
 
